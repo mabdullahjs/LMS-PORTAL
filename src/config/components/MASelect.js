a8a6ab28482594825a4c144716d18c062b53e9c6
Muhammad Abdullah <mabdullah2037@outlook.com>
2023-03-24 17:21:27 +0500

    form screen completed

--- src/config/components/MASelect.js
+++ src/config/components/MASelect.js
@@ -1,65 +1,32 @@
-
-
-import * as React from "react";
-import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import {Get} from "../apimethod/Apimethod" 
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 export default function MASelect(props) {
-  const {
-    value,
-    datasource,
-    onChange,
-    label,
-    variant,
-    disabled,
-    apiName,
-    displayField,
-    valueField,
-  } = props;
+  const {value , onChange , option , label , className} = props
 
-  const [apiList, SetApiList] = React.useState(
-    datasource ? [...datasource] : []
-  );
 
-  React.useEffect(() => {
-    if (apiName) {
-      Get(apiName)
-        .then((res) => {
-          SetApiList([...res]);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, []);
   return (
-    <Box sx={{ minWidth: 120 }}>
-      <FormControl variant={variant ? variant : "standard"} fullWidth>
+    <Box className={className} sx={{ minWidth: 220 }}>
+      <FormControl fullWidth>
         <InputLabel id="demo-simple-select-label">{label}</InputLabel>
         <Select
-          disabled={disabled}
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           value={value}
           label="Age"
           onChange={onChange}
+          
         >
-          {apiList && Array.isArray(apiList)
-            ? apiList.map((e, i) => (
-                <MenuItem
-                  key={i}
-                  value={
-                    valueField ? e[valueField ? valueField : "valueField"] : e
-                  }
-                >
-                  {e[displayField ? displayField : "displayField"]}
-                </MenuItem>
-              ))
-            : null}
+          {/* <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem> */}
+          {option.map((item , index)=>{
+            return <MenuItem key={index} value={item}>{item}</MenuItem>
+          })}
         </Select>
       </FormControl>
     </Box>
--- src/pages/homeScreens/InsForm.js
+++ src/pages/homeScreens/InsForm.js
@@ -1,31 +1,35 @@
-import { Grid } from '@mui/material'
-import { Box } from '@mui/system'
-import React from 'react'
-import { Col, Row } from 'react-bootstrap'
-import MAInput from '../../config/components/MAInput'
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
+import MAInput from "../../config/components/MAInput";
+import MASelect from "../../config/components/MASelect";
+import MAButton from "../../config/components/MAButton";
 
 function InsForm() {
-    return (
-        <Box className=" container">
-            <Row>
-                <MAInput width="30%" label="name"/>
-                <MAInput width="30%" label="name"/>
-            </Row>
-            <Row>
-                <Col>1 of 2</Col>
-                <Col>2 of 2</Col>
-            </Row>
-            <Row>
-                <Col>1 of 2</Col>
-                <Col>2 of 2</Col>
-            </Row>
-            <Row>
-                <Col>1 of 2</Col>
-                <Col>2 of 2</Col>
-            </Row>
-        </Box>
+    // userType state
+    const [user , setUser] = useState("");
+    const [type , setType] = useState("");
+  return (
+    <Box className="container mt-5">
+      <Typography className="text-center" variant="h5">
+        CREATE INSTITUTES
+      </Typography>
+      <Box className="mt-3 d-flex justify-content-evenly flex-wrap">
+        <MAInput className="m-3" variant="outlined" label="Name" />
+        <MAInput className="m-3" variant="outlined" label="No of campus" />
+        <MAInput className="m-3" variant="outlined" label="Location" />
+        <MAInput className="m-3" variant="outlined" label="Address" />
+        <MAInput className="m-3" variant="outlined" label="Contact" />
+        <MAInput className="m-3" variant="outlined" label="Owner Contact" />
+        <MASelect className="m-3"  value={user} onChange={(e)=>setUser(e.target.value)} option={['institute']} label="user"/>
+        <MASelect className="m-3"  value={type} onChange={(e)=>setType(e.target.value)} option={['School' , 'College' , 'University' , 'Institute']} label="Type"/>
+      </Box>
 
-    )
+      <Box className="d-flex justify-content-center mt-5">
+      <MAButton label="create Insitute"/>
+      </Box>
+    </Box>
+  );
 }
 
-export default InsForm
+export default InsForm;
