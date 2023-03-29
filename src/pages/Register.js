3a5763516c22ac2b769cb7caea78f360f8e323b9
mabdullah <mabdullah2037@gmail.com>
2023-03-29 18:37:35 +0500

    register form data send to database

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,57 +1,105 @@
 import { Grid, Typography } from '@mui/material'
 import { Box } from '@mui/system'
-import React from 'react'
+import React, { useState } from 'react'
 import MAInput from '../config/components/MAInput'
 import MASelect from '../config/components/MASelect'
+import MAButton from '../config/components/MAButton'
 import MADatePicker from '../config/components/MADataPicker'
+import { sendData } from '../config/Firebase/firebaseMethod'
 
 function Register() {
+    // form state
+    const [city, setCity] = useState("");
+    const [course, setCourse] = useState("");
+    const [name, setName] = useState("");
+    const [fname, setFname] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [cnic, setCnic] = useState("");
+    const [fcnic, setFcnic] = useState("");
+    const [date, setDate] = useState("");
+    const [gender, setGender] = useState("");
+    const [address, setAddress] = useState("");
+    const [qualification, setQualification] = useState("");
+    const [laptop, setLaptop] = useState("");
+    const [loader , setLoader] = useState(false)
+
+    // register student
+    const registerStudent = ()=>{
+        setLoader(true);
+        const obj = {
+            name:name,
+            FatherName:fname,
+            Email : email,
+            phone:phone,
+            cnic:cnic,
+            FatherCnic:fcnic,
+            Gender:gender,
+            BirthDate:date.$d,
+            Address:address,
+            Qualification:qualification,
+            Laptop:laptop,
+        }
+        sendData(obj , "NewAdmission")
+        .then((res)=>{
+            console.log(res);
+            setLoader(false);
+        })
+        .catch((err)=>{
+            console.log(err);
+            setLoader(false);
+        })
+    }
     return (
         <Box>
             <Typography className='text-center mt-3' variant='h5'>Registration Form</Typography>
             <Box className="mt-5 d-flex justify-content-center">
                 <Grid className='w-50' container spacing={5}>
                     <Grid className='d-flex justify-content-center' item xs={12} md={6}>
-                        <MASelect variant="outlined" label="Select City" />
+                        <MASelect option={["Karachi", "Lahore" , "Faislabad"]} onChange={(e) => setCity(e.target.value)} value={city} variant="outlined" label="Select City"  />
                     </Grid>
                     <Grid className='d-flex justify-content-center' item xs={12} md={6}>
-                        <MASelect variant="outlined" label="Select Course" />
+                        <MASelect option={["TypeScript", "JavaScript"]} onChange={(e) => setCourse(e.target.value)} value={course} variant="outlined" label="Select Course" />
                     </Grid>
                     <Grid className='d-flex justify-content-center' item xs={12} md={6}>
-                        <MAInput variant="outlined" label="Full Name" />
+                        <MAInput onChange={(e) => setName(e.target.value)} value={name} variant="outlined" label="Full Name" />
                     </Grid>
                     <Grid className='d-flex justify-content-center' item xs={12} md={6}>
-                        <MAInput variant="outlined" label="Father Name" />
+                        <MAInput onChange={(e) => setFname(e.target.value)} value={fname} variant="outlined" label="Father Name" />
                     </Grid>
                     <Grid className='d-flex justify-content-center' item xs={12} md={6}>
-                        <MAInput variant="outlined" label="Email" />
+                        <MAInput onChange={(e) => setEmail(e.target.value)} type="email" value={email} variant="outlined" label="Email" />
                     </Grid>
                     <Grid className='d-flex justify-content-center' item xs={12} md={6}>
-                        <MAInput variant="outlined" type="number" label="Phone" />
+                        <MAInput onChange={(e) => setPhone(e.target.value)} value={phone} variant="outlined" type="number" label="Phone" />
                     </Grid>
                     <Grid className='d-flex justify-content-center' item xs={12} md={6}>
-                        <MAInput variant="outlined" type="number" label="CNIC" />
+                        <MAInput onChange={(e) => setCnic(e.target.value)} value={cnic} variant="outlined" type="number" label="CNIC" />
                     </Grid>
                     <Grid className='d-flex justify-content-center' item xs={12} md={6}>
-                        <MAInput variant="outlined" type="number" label="Father CNIC" />
+                        <MAInput onChange={(e) => setFcnic(e.target.value)} value={fcnic} variant="outlined" type="number" label="Father CNIC" />
                     </Grid>
                     <Grid className='d-flex justify-content-center' item xs={12} md={6}>
-                        <MADatePicker variant="outlined" label="mm/dd/yyyy" />
+                        <MADatePicker onChange={(e) => setDate(e)} value={date} variant="outlined" label="Date of Birth" />
                     </Grid>
                     <Grid className='d-flex justify-content-center' item xs={12} md={6}>
-                        <MASelect option={["Male", "Female"]} variant="outlined" label="Select Gender" />
+                        <MASelect onChange={(e) => setGender(e.target.value)} value={gender} option={["Male", "Female"]} variant="outlined" label="Select Gender" />
                     </Grid>
                     <Grid className='d-flex justify-content-center' item xs={12} md={12}>
-                        <MAInput variant="outlined" label="Address" />
+                        <MAInput onChange={(e) => setAddress(e.target.value)} value={address} variant="outlined" label="Address" />
                     </Grid>
                     <Grid className='d-flex justify-content-center' item xs={12} md={12}>
-                    <MAInput variant="outlined" label="Last Qualification" />
+                        <MAInput onChange={(e) => setQualification(e.target.value)} value={qualification} variant="outlined" label="Last Qualification" />
                     </Grid>
                     <Grid className='d-flex justify-content-center' item xs={12} md={12}>
-                    <MAInput variant="outlined" label="Do you have a laptop? " />
+                        <MAInput onChange={(e) => setLaptop(e.target.value)} value={laptop} variant="outlined" label="Do you have a laptop? " />
                     </Grid>
 
                 </Grid>
+
+            </Box >
+            <Box className="mt-5 d-flex justify-content-center">
+                <MAButton onClick={()=>registerStudent()} loading={loader} label="Submit" />
             </Box>
         </Box>
     )
